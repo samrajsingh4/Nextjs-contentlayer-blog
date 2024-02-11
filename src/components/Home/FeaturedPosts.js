c77eacdc84899ef938608639d643e1ff1b2ace98
samrajsingh4 <samrajsingh4@gmail.com>
2024-02-11 18:00:13 +0000

    Part 2

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -9,14 +9,14 @@ const FeaturedPosts = ({ blogs }) => {
     <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">Featured Posts</h2>
 
     <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
-      <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
+      <article className="col-span-1 row-span-2 relative">
         <BlogLayoutOne blog={sortedBlogs[1]} />
       </article>
-      <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
+      <article className="col-span-1 row-span-1 relative">
                 <BlogLayoutTwo blog={sortedBlogs[2]} />
 
       </article>
-      <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+      <article className="col-span-1 row-span-1 relative">
                 <BlogLayoutTwo blog={sortedBlogs[3]} />
 
       </article>
