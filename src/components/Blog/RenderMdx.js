2303eb8f423d5f34112cf11e9ea911344d879d75
samrajsingh4 <samrajsingh4@gmail.com>
2024-02-11 19:17:32 +0000

    5

--- src/components/Blog/RenderMdx.js
+++ src/components/Blog/RenderMdx.js
@@ -13,7 +13,7 @@ const RenderMdx = ({blog}) => {
     const MDXContent = useMDXComponent(blog.body.code)
 
   return (
-    <div className='col-span-8 font-in prose prose-lg max-w-max
+    <div className='col-span-12  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
     prose-blockquote:bg-accent/20 
     prose-blockquote:p-2
     prose-blockquote:px-6
@@ -22,6 +22,9 @@ const RenderMdx = ({blog}) => {
     prose-blockquote:rounded-r-lg
     
     prose-li:marker:text-accent
+
+    first-letter:text-3xl
+    sm:first-letter:text-5xl
 
 
     '> 
