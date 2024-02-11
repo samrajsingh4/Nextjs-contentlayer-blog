c5f6c72f416c97a6f58d2901f7adf851f9a582db
samrajsingh4 <samrajsingh4@gmail.com>
2024-02-11 19:30:30 +0000

    Base Done

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,10 +1,11 @@
+import BlogDetails from "@/src/components/Blog/BlogDetails";
+import RenderMdx from "@/src/components/Blog/RenderMdx";
 import Tag from "@/src/components/Elements/Tag";
+import siteMetadata from "@/src/utils/siteMetaData";
 import { allBlogs } from "contentlayer/generated";
 import { slug } from "github-slugger";
 import Image from "next/image";
-import BlogDetails from "@/src/components/Blog/BlogDetails";
-import RenderMdx from "@/src/components/Blog/RenderMdx";
-import siteMetadata from "@/src/utils/siteMetaData";
+import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
   return allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
@@ -57,39 +58,72 @@ export async function generateMetadata({ params }) {
 }
 
 export default function BlogPage({ params }) {
+  const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
 
-    const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
+  if(!blog){
+    notFound()
+  }
 
-    return <article>
-        <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
+
+  let imageList = [siteMetadata.socialBanner];
+  if (blog.image) {
+    imageList =
+      typeof blog.image.filePath === "string"
+        ? [siteMetadata.siteUrl + blog.image.filePath.replace("../public", "")]
+        : blog.image;
+  }
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    "headline": blog.title,
+    "description": blog.description,
+    "image": imageList,
+    "datePublished": new Date(blog.publishedAt).toISOString(),
+    "dateModified": new Date(blog.updatedAt || blog.publishedAt).toISOString(),
+    "author": [{
+        "@type": "Person",
+        "name": blog?.author ? [blog.author] : siteMetadata.author,
+        "url": siteMetadata.twitter,
+      }]
+  }
+
+  return (
+    <>
+    <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+       <article>
+      <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
         <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Tag
+          <Tag
             name={blog.tags[0]}
             link={`/categories/${slug(blog.tags[0])}`}
             className="px-6 text-sm py-2"
-            />
-            <h1
+          />
+          <h1
             className="inline-block mt-6 font-semibold capitalize text-light text-2xl md:text-3xl lg:text-5xl !leading-normal relative w-5/6"
-            >
+          >
             {blog.title}
-            </h1>
+          </h1>
         </div>
         <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-dark/60 dark:bg-dark/40" />
         <Image
-            src={blog.image.filePath.replace("../public", "")}
-            placeholder="blur"
-            blurDataURL={blog.image.blurhashDataUrl}
-            alt={blog.title}
-            width={blog.image.width}
-            height={blog.image.height}
-            className="aspect-square w-full h-full object-cover object-center"
-            priority
-            sizes="100vw"
+          src={blog.image.filePath.replace("../public", "")}
+          placeholder="blur"
+          blurDataURL={blog.image.blurhashDataUrl}
+          alt={blog.title}
+          width={blog.image.width}
+          height={blog.image.height}
+          className="aspect-square w-full h-full object-cover object-center"
+          priority
+          sizes="100vw"
         />
-        </div>
-        <BlogDetails blog={blog} slug={params.slug} />
+      </div>
+      <BlogDetails blog={blog} slug={params.slug} />
 
-        <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
+      <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
         <div className="col-span-12  lg:col-span-4">
           <details
             className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
@@ -126,9 +160,10 @@ export default function BlogPage({ params }) {
             </ul>
           </details>
         </div>
-            <RenderMdx blog={blog} />
-        </div>
-
-
-    </article>  
+        <RenderMdx blog={blog} />
+      </div>
+    </article>
+    </>
+   
+  );
 }
