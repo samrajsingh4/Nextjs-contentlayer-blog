5f757c277b2b834ab2cc78c9e01963427c113cce
samrajsingh4 <samrajsingh4@gmail.com>
2024-02-12 17:57:11 +0000

    j

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link";
-import Logo from "./Logo";
+import Logo from "./logo";
 import { LinkedinIcon, GithubIcon, TwitterIcon, DribbbleIcon } from "../Icons";
 import siteMetadata from "@/src/utils/siteMetaData";
 import { useState } from "react";
@@ -71,9 +71,7 @@ const Header = () => {
 
       <div className="hidden sm:flex items-center">
         <a href={siteMetadata.linkedin} rel="noopener noreferrer" className="inline-block w-6 h-6 mr-4" aria-label="Reach out to me via LinkedIn" target="_blank"><LinkedinIcon className="hover:scale-125 transition-all ease duration-200" /></a>
-        <a href={siteMetadata.twitter} rel="noopener noreferrer" className="inline-block w-6 h-6 mr-4" aria-label="Reach out to me via Twitter" target="_blank"><TwitterIcon className="hover:scale-125 transition-all ease duration-200" /></a>
         <a href={siteMetadata.github} rel="noopener noreferrer" className="inline-block w-6 h-6 mr-4" aria-label="Check my profile on Github" target="_blank"><GithubIcon className="hover:scale-125 transition-all ease duration-200 dark:fill-light" /></a>
-        <a href={siteMetadata.dribbble} rel="noopener noreferrer" className="inline-block w-6 h-6 mr-4" aria-label="Check my profile on Dribbble" target="_blank"><DribbbleIcon className="hover:scale-125 transition-all ease duration-200" /></a>
       </div>
     </header>
   );
